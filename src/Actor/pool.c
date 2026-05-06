c9bf1e15ab4ded8f0c9460bd51c0e5bf529032d1
Victor Morrow <victor.j.morrow@gmail.com>
2026-05-06 16:15:30 -0400

    feat: add three-tier pool allocator (OFFS-7)
    Thread-local LIFO free lists for lock-free hot path, mutex-protected
    global free lists for cross-thread transfers, slab carving and
    posix_memalign fallback. 8 size classes: 16-2048 bytes.

---
+++ src/Actor/pool.c
@@ -0,0 +1,140 @@
+//
+// Created by victor on 5/6/25.
+//
+
+#include "pool.h"
+#include "../Util/allocator.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+static pool_global_t _pool_global[POOL_COUNT];
+static _Thread_local pool_local_t _pool_local[POOL_COUNT];
+
+static void _pool_global_init(void) {
+  for (size_t index = 0; index < POOL_COUNT; index++) {
+    size_t used_size = pool_used_size(index);
+    size_t threshold = POOL_ALIGN / used_size;
+    if (threshold < POOL_MIN_THRESHOLD) {
+      threshold = POOL_MIN_THRESHOLD;
+    }
+    if (threshold > POOL_MAX_THRESHOLD) {
+      threshold = POOL_MAX_THRESHOLD;
+    }
+    _pool_global[index].head = NULL;
+    _pool_global[index].count = threshold;
+    platform_lock_init(&_pool_global[index].lock);
+  }
+}
+
+static pthread_once_t _pool_init_once = PTHREAD_ONCE_INIT;
+
+static void _pool_global_init_once(void) {
+  pthread_once(&_pool_init_once, _pool_global_init);
+}
+
+static void _pool_push_global(size_t index, pool_item_t* list_head) {
+  platform_lock(&_pool_global[index].lock);
+  list_head->next = _pool_global[index].head;
+  _pool_global[index].head = list_head;
+  platform_unlock(&_pool_global[index].lock);
+}
+
+static pool_item_t* _pool_pull_global(size_t index) {
+  platform_lock(&_pool_global[index].lock);
+  pool_item_t* head = _pool_global[index].head;
+  if (head != NULL) {
+    _pool_global[index].head = NULL;
+  }
+  platform_unlock(&_pool_global[index].lock);
+  return head;
+}
+
+static void* _pool_alloc_block(size_t index) {
+  size_t used_size = pool_used_size(index);
+  if (used_size < POOL_ALIGN) {
+    pool_local_t* local = &_pool_local[index];
+    if (local->start == NULL || local->start >= local->end) {
+      size_t block_size = POOL_ALIGN;
+      local->start = get_clear_memory(block_size);
+      local->end = local->start + block_size;
+    }
+    void* pointer = local->start;
+    local->start += used_size;
+    return pointer;
+  }
+  void* pointer = NULL;
+  if (posix_memalign(&pointer, POOL_ALIGN, used_size) != 0) {
+    abort();
+  }
+  memset(pointer, 0, used_size);
+  return pointer;
+}
+
+size_t pool_index(size_t size) {
+  if (size == 0) {
+    size = 1;
+  }
+  size_t bits = POOL_MIN_BITS;
+  while ((1UL << bits) < size) {
+    bits++;
+  }
+  size_t index = bits - POOL_MIN_BITS;
+  if (index >= POOL_COUNT) {
+    index = POOL_COUNT - 1;
+  }
+  return index;
+}
+
+size_t pool_used_size(size_t index) {
+  return (1UL << (POOL_MIN_BITS + index));
+}
+
+void* pool_alloc(size_t index) {
+  _pool_global_init_once();
+  pool_local_t* local = &_pool_local[index];
+  if (local->head != NULL) {
+    pool_item_t* item = local->head;
+    local->head = item->next;
+    local->length--;
+    return item;
+  }
+  pool_item_t* global_head = _pool_pull_global(index);
+  if (global_head != NULL) {
+    local->head = global_head->next;
+    local->length = 0;
+    return global_head;
+  }
+  return _pool_alloc_block(index);
+}
+
+void pool_free(size_t index, void* pointer) {
+  if (pointer == NULL) {
+    return;
+  }
+  _pool_global_init_once();
+  pool_item_t* item = (pool_item_t*)pointer;
+  pool_local_t* local = &_pool_local[index];
+  item->next = local->head;
+  local->head = item;
+  local->length++;
+  if (local->length >= _pool_global[index].count) {
+    pool_item_t* list_head = local->head;
+    local->head = NULL;
+    local->length = 0;
+    _pool_push_global(index, list_head);
+  }
+}
+
+void pool_thread_cleanup(void) {
+  for (size_t index = 0; index < POOL_COUNT; index++) {
+    pool_local_t* local = &_pool_local[index];
+    if (local->head != NULL) {
+      _pool_push_global(index, local->head);
+      local->head = NULL;
+      local->length = 0;
+    }
+    local->start = NULL;
+    local->end = NULL;
+  }
+}
---
+++ src/Actor/pool.h
@@ -0,0 +1,45 @@
+//
+// Created by victor on 5/6/25.
+//
+
+#ifndef OFFS_POOL_H
+#define OFFS_POOL_H
+
+#include "../Util/threadding.h"
+#include <stddef.h>
+#include <stdint.h>
+
+#define POOL_MIN_BITS     4
+#define POOL_MAX_BITS     11
+#define POOL_COUNT        (POOL_MAX_BITS - POOL_MIN_BITS + 1)
+#define POOL_ALIGN        1024
+#define POOL_MIN_THRESHOLD 32
+#define POOL_MAX_THRESHOLD 1024
+
+typedef struct pool_item_t {
+  struct pool_item_t* next;
+} pool_item_t;
+
+typedef struct pool_local_t {
+  pool_item_t* head;
+  size_t length;
+  char* start;
+  char* end;
+} pool_local_t;
+
+typedef struct pool_global_t {
+  pool_item_t* head;
+  size_t count;
+  PLATFORMLOCKTYPE(lock);
+} pool_global_t;
+
+void*  pool_alloc(size_t index);
+void   pool_free(size_t index, void* pointer);
+size_t pool_index(size_t size);
+size_t pool_used_size(size_t index);
+void   pool_thread_cleanup(void);
+
+#define POOL_ALLOC(TYPE)  ((TYPE*)pool_alloc(pool_index(sizeof(TYPE))))
+#define POOL_FREE(TYPE, VALUE)  pool_free(pool_index(sizeof(TYPE)), (VALUE))
+
+#endif // OFFS_POOL_H
