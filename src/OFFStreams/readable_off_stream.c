2b04de5f63086a9f2db65e53700b56a5c18c6e65
Victor Morrow <victor.j.morrow@gmail.com>
2026-09-25 17:51:50 -0400

    debug: stream-layer traces for ACI

--- src/OFFStreams/readable_off_stream.c
+++ src/OFFStreams/readable_off_stream.c
@@ -2,6 +2,7 @@
 // Created by victor on 5/7/26.
 //
 
+#include <stdio.h>
 #include "readable_off_stream.h"
 #include "../Util/allocator.h"
 #include "../Util/error.h"
@@ -391,6 +392,7 @@ void readable_off_stream_dispatch(void* state, message_t* msg) {
       }
 
       if (result->block == NULL) {
+        fprintf(stderr, "RS-TRACE: block miss, network=%d\n", stream->network != NULL);
         /* Block not found in cache */
         if (stream->network != NULL) {
           /* Network-aware: send NETWORK_LOCAL_FIND_BLOCK for this specific hash.
